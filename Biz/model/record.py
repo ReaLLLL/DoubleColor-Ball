67a753f2f7faa0e3723f05c33821fa9c88b8db5a
Alex Song <songyu@u51.com>
2017-11-30 23:54:42 +0800

    实现从mongodb数据库存取数据

--- Biz/model/record.py
+++ Biz/model/record.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 import xlrd
 
+
 class Record:
     def __init__(self, no, date, red1, red2, red3, red4, red5, red6, blue):
         self.no = no
@@ -17,4 +18,5 @@ class Record:
     def __repr__(self):
         year, month, day, hour, minute, second = xlrd.xldate_as_tuple(self.date, 0)
         date = datetime(year, month, day, hour, minute, second)
-        return '{"no": "%s", "weekday": %d, "red1": %d, "red2": %d, "red3": %d, "red4": %d, "red5": %d, "red6": %d, "blue": %d}'%(self.no, date.weekday()+1, self.red1, self.red2, self.red3, self.red4, self.red5, self.red6, self.blue)
+        return '{"no": "%s", "weekday": %d, "red1": %d, "red2": %d, "red3": %d, "red4": %d, "red5": %d, "red6": %d, "blue": %d}'\
+               %(self.no, date.weekday()+1, self.red1, self.red2, self.red3, self.red4, self.red5, self.red6, self.blue)
--- Biz/util/fileUtil.py
+++ Biz/util/fileUtil.py
@@ -5,10 +5,25 @@ from pymongo import MongoClient
 import json
 
 
-data = xlrd.open_workbook('/Users/alexsong/Downloads/DCB.xls')
-table = data.sheet_by_name(u'sheet1')
-client = MongoClient('localhost', 27017)
-db = client.get_database('DOUBLECOLOR_BALL')
-for i in range(0, table.nrows):
-    myrecord=record.Record(table.cell_value(i,0),table.cell_value(i,1),table.cell_value(i,2),table.cell_value(i,3),table.cell_value(i,4),table.cell_value(i,5),table.cell_value(i,6),table.cell_value(i,7),table.cell_value(i,8))
-    db.col.insert(json.loads(myrecord.__repr__()))
+def load_data_to_db(file_name, db_name):
+    data = xlrd.open_workbook(file_name)
+    table = data.sheet_by_name(u'sheet1')
+    client = MongoClient('localhost', 27017)
+    db = client.get_database(db_name)
+    for i in range(0, table.nrows):
+        my_record=record.Record(table.cell_value(i,0),table.cell_value(i,1),table.cell_value(i,2),table.cell_value(i,3),table.cell_value(i,4),table.cell_value(i,5),table.cell_value(i,6),table.cell_value(i,7),table.cell_value(i,8))
+        db.col.insert(json.loads(my_record.__repr__()))
+
+
+def get_data_from_db_aggregate(db_name, pipeline):
+    client = MongoClient('localhost', 27017)
+    db = client.get_database(db_name)
+#    result = db.col.aggregate([{'$group' : {'_id' : {'weekday':'$weekday', 'blue':'$blue'},
+    # "totalCount" : {'$sum' : 1}}}])
+    result = db.col.aggregate(pipeline)
+    return list(result)
+
+
+
+
+
